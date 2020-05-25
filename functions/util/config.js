a8d0f7cf0e0aa3ed12b01077126a4ccfa117ff70
chr-ge <61952924+chr-ge@users.noreply.github.com>
2020-05-25 14:55:32 -0400

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    apiKey: "AIzaSyA130vgw_RQfMM4DjTsSayN0u3MpcGUcDM",
-    authDomain: "internshipplanner.firebaseapp.com",
-    databaseURL: "https://internshipplanner.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "internshipplanner",
-    storageBucket: "internshipplanner.appspot.com",
-    messagingSenderId: "917680198983",
-    appId: "1:917680198983:web:90ff06f3590dd3f2986862"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
